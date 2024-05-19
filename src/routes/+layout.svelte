7326ed5806394ab05d8d763a3d4b12dabadff65c
Abhijeet Sonar <abhijeet.nkt@gmail.com>
2024-05-19 16:00:00 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<slot />
+
+<style lang="postcss">
+	:global(html) {
+		font-family: 'IBM Plex Mono', monospace;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<!-- Hero -->
+<div id="home" class="max-w-screen-md mx-auto flex">
+	<div class="w-full flex lg:mx-0 mx-6 lg:flex-row flex-col justify-between gap-14">
+		<div id="avatar" class="w-52 h-52 bg-no-repeat bg-cover" />
+
+		<div class="flex flex-col justify-center gap-5">
+			<p>Hi 👋 I am</p>
+			<p class="text-6xl font-bold underline">Abhijeet Sonar</p>
+			<p class="">A motivated software developer</p>
+		</div>
+	</div>
+</div>
+
+<div id="socials"></div>
+
+<div id="projects"></div>
+
+<div id="blogs"></div>
+
+<style>
+	#avatar {
+		background-image: url(https://vignette.wikia.nocookie.net/konosuba/images/7/7b/Aqua_Artwork_03.png/revision/latest?cb%5Cu003d20180608054657);
+	}
+</style>
